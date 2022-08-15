0432daa5aecf0b3f468d3f265454cad78db457d0
Surya10d <104970523+Surya10d@users.noreply.github.com>
2022-08-15 08:00:19 +0530

    Update test_one.py

--- test_one.py
+++ test_one.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def test_one():
-    auth_token = f"abcd {os.environ.get("TOKEN")}"
+    auth_token = f"abcd {os.environ.get('TOKEN')}"
     print(os.environ.get("AUTH_TOKEN"))
     print(auth_token.replace(" ",""))
     # ff = open("env.txt", "r")
