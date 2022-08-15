726590b64fe0db787f0223ef315f0d36babd3b47
surya10d <surya.mr@10decoders.in>
2022-08-15 11:10:42 +0530

    modify test

--- test_one.py
+++ test_one.py
@@ -5,7 +5,7 @@ import subprocess
 def test_one():
     print(len(os.environ.get("AUTH_TOKEN")))
     print("token="+repr(os.environ.get("AUTH_TOKEN")))
-    print("True")
+    print(True)
     # ff = open("env.txt", "r")
     # print(ff.read())
     assert 1 == 2
