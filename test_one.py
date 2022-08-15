d028bd81680065d04b00e643e55565e879f64be6
surya10d <surya.mr@10decoders.in>
2022-08-15 11:14:41 +0530

    modify test

--- test_one.py
+++ test_one.py
@@ -3,9 +3,7 @@ import subprocess
 
 
 def test_one():
-    print("token="+repr(os.environ.get("AUTH_TOKEN")))
-    print(True)
-    if os.environ.get("AUTH_TOKEN") == "c3VyeWEubXIramlyYTFAMTBkZWNvZGVycy5pbjpnRTByeFptRzBEalQ0SXo3TkU4YUYzM0Y=":
+    if str(os.environ.get("AUTH_TOKEN")) == "c3VyeWEubXIramlyYTFAMTBkZWNvZGVycy5pbjpnRTByeFptRzBEalQ0SXo3TkU4YUYzM0Y=":
         print("ENV value checked")
     # ff = open("env.txt", "r")
     # print(ff.read())
