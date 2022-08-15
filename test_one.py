a9a534ed839fe12bccda7e070d77f6188ff2d50f
surya10d <surya.mr@10decoders.in>
2022-08-15 11:24:29 +0530

    modify test

--- test_one.py
+++ test_one.py
@@ -8,7 +8,7 @@ def test_one():
 
     token = os.environ.get("AUTH_TOKEN")
     headers = {
-        'Authorization': f'Basic {token}',
+        'Authorization': "Basic "+token,
         'Content-Type': 'application/json'
     }
 
