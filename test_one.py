651a17cd4cb079ec1a93d12dcaaf1bf7cb603f28
Surya10d <104970523+Surya10d@users.noreply.github.com>
2022-08-15 07:44:10 +0530

    Update test_one.py

--- test_one.py
+++ test_one.py
@@ -3,7 +3,8 @@ import subprocess
 
 
 def test_one():
-    print(os.environ.get("AUTH_TOKEN"), "authtoken")
+    auth_token = os.environ.get("AUTH_TOKEN")
+    print(str(auth_token).replace(" ",""))
     # ff = open("env.txt", "r")
     # print(ff.read())
     assert 1 == 2
