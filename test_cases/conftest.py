1ceaf2d31fadb8097630f75055f80a0588bc8d68
surya10d <surya.mr@10decoders.in>
2022-09-27 19:57:12 +0530

    Modified the xray test case import

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -20,17 +20,19 @@ pytest_id = []
 pytest_status = []
 pytest_id_value = None
 AUTH_TOKEN = None
-PREFIX_TICKET_VALUE = None
 JIRA_DOMAIN = None
 PASS_STATUS_TRANSITION = None
 FAIL_STATUS_TRANSITION = None
 JIRA_CONDITION = None
+TEST_EXECUTION_NAME = None
 CLIENT_ID = None
 CLIENT_SECRET = None
+TESTCASE_UPDATE = None
 testcase_comment = None
 img_path = None
 project_key = {}
 jira_api_endpoint = ".atlassian.net/rest/api/2/issue"
+timestamp = ""
 
 
 def pytest_cmdline_main(config):
@@ -42,6 +44,8 @@ def pytest_cmdline_main(config):
             report file path = ./reports/<year>/<month>/<date>/
             report file name = test_report_<timestamp>.html
     """
+    global timestamp
+
     report_time = datetime.now()
     year = datetime.strftime(report_time, '%Y')
     month = datetime.strftime(report_time, '%m')
@@ -131,19 +135,19 @@ def pytest_collection_modifyitems(session, config, items):
 
 
 def get_env_values():
-    global AUTH_TOKEN, PREFIX_TICKET_VALUE, JIRA_DOMAIN, PASS_STATUS_TRANSITION, FAIL_STATUS_TRANSITION, JIRA_CONDITION, CLIENT_ID, CLIENT_SECRET
+    global AUTH_TOKEN, JIRA_DOMAIN, PASS_STATUS_TRANSITION, FAIL_STATUS_TRANSITION, JIRA_CONDITION, CLIENT_ID, CLIENT_SECRET, TEST_EXECUTION_NAME, TESTCASE_UPDATE
 
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
-    PREFIX_TICKET_VALUE = os.environ.get("PREFIX_TICKET_VALUE")
     JIRA_DOMAIN = os.environ.get("JIRA_DOMAIN")
     PASS_STATUS_TRANSITION = os.environ.get("PASS_STATUS_TRANSITION")
     FAIL_STATUS_TRANSITION = os.environ.get("FAIL_STATUS_TRANSITION")
     JIRA_CONDITION = os.environ.get("JIRA_CONDITION", None)
     CLIENT_ID = os.environ.get("CLIENT_ID")
     CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-
+    TEST_EXECUTION_NAME = os.environ.get("TEST_EXECUTION_NAME")
+    TESTCASE_UPDATE = os.environ.get("TESTCASE_UPDATE")
 
 
 def setup_module(module):
@@ -172,66 +176,64 @@ def pytest_runtest_makereport(item):
     result = outcome.get_result()
 
     log_file = "test_results.log"
-    if result.when == "call" and JIRA_CONDITION:
+    if result.when == "call":
         try:
             with open(log_file, "a") as f:
                 ticket_id = next(pytest_id_value)
                 f.write(result.nodeid + "   " + result.outcome + "   " + str(result.duration)+" Ticket ID - "+ticket_id+"\n")
                 proj_key = ticket_id.split("-")[0]
+            if JIRA_CONDITION:
                 if proj_key in project_key.keys():
-                    execute_testcase_in_testexecution(project_key[proj_key], ticket_id, result.outcome)
+                    add_test_cases_in_test_execution(project_key[proj_key], ticket_id, result.outcome)
                 else:
                     project_id = get_project_id(proj_key)
-                    issue_type_id = get_issue_type_id(proj_key, "Test Execution")
-                    test_exec_key = create_ticket(project_id, issue_type_id)
+                    issue_type_id = get_issue_type_id(proj_key, TEST_EXECUTION_NAME)
+                    test_exec_key = create_test_execution_ticket_in_jira(project_id, issue_type_id)
                     project_key[proj_key] = test_exec_key
-                    execute_testcase_in_testexecution(project_key[proj_key], ticket_id, result.outcome)
-            # ticket_id = next(pytest_id_value)
-            # if ticket_id in result.nodeid:
-            #     send_results_to_jira(result, ticket_id)
-            # elif ticket_id not in result.nodeid:
-            #     ticket_id = f"{PREFIX_TICKET_VALUE}"+(result.nodeid.replace("_", "-")).split(f"{PREFIX_TICKET_VALUE}")[1][:-1]
-            #     send_results_to_jira(result, ticket_id)
+                    add_test_cases_in_test_execution(project_key[proj_key], ticket_id, result.outcome)
         except Exception as e:
-            print("Error", e)
+            print("Error occurred in making report", e)
             pass
-    collect_screenshot(item, result)
+    # collect_screenshot(item, result)
 
 
-def authenticate_xray_api():
+def get_authentication_token_for_xray_api():
     auth_cred = { "client_id": CLIENT_ID,"client_secret": CLIENT_SECRET }
     token = requests.request(url="https://xray.cloud.getxray.app/api/v1/authenticate", method="POST",
                      json=auth_cred)
     return json.loads(token.text)
 
 
-def execute_testcase_in_testexecution(key, test_case_id, status):
+def add_test_cases_in_test_execution(key, test_case_id, status):
     try:
-        token = authenticate_xray_api()
-        headers = {"Authorization": f"Bearer {str(token)}",
+        token = get_authentication_token_for_xray_api()
+        headers = {"Authorization": f"Bearer {token}",
                    "Content-Type": "application/json"}
         payload = {
         "testExecutionKey": key,
-        "info" : {
-            "summary" : "Execution of automated tests for release v1.3",
-            "description" : "This execution is automatically created when importing execution results from an external source",
+        "info": {
+            "summary": f"Execution of automated tests at {timestamp}",
+            "description": "This execution is automatically created when importing execution results from an external "
+                           "source",
         },
-        "tests" : [
-            {
+        "tests": [
+                {
                 "testKey": test_case_id,
                 "comment": "Successful execution" if status == "passed" else "Execution failed",
                 "status": status.upper()
-            }
-        ]
-    }
+                }
+            ]
+        }
         requests.request(url="https://xray.cloud.xpand-it.com/api/v1/import/execution", method="POST", headers=headers,
                          json=payload, verify=False)
+        if TESTCASE_UPDATE:
+            send_results_to_jira(status, test_case_id, key)
     except Exception as e:
         logger.info("Exception occured when linking test case to test execution", e)
 
 
-def create_ticket(project_id, issue_type_id, summary="Test Execution summary",
-                  description="Description of the Test Execution"):
+def create_test_execution_ticket_in_jira(project_id, issue_type_id, summary="Automated Test Execution summary",
+                                         description="Description of the Test Execution"):
     payload = {
     "fields": {
         "project": {
@@ -244,7 +246,7 @@ def create_ticket(project_id, issue_type_id, summary="Test Execution summary",
             }
         }
     }
-    headers = {"Authorization": "Basic c3VyeWEubXIramlyYTFAMTBkZWNvZGVycy5pbjpnRTByeFptRzBEalQ0SXo3TkU4YUYzM0Y=",
+    headers = {"Authorization": f"Basic {AUTH_TOKEN}",
                "Content-Type": "application/json"}
     url = f"https://{JIRA_DOMAIN}{jira_api_endpoint}"
     response = requests.request(url=url, method="POST", json=payload, headers=headers)
@@ -253,10 +255,13 @@ def create_ticket(project_id, issue_type_id, summary="Test Execution summary",
 
 
 def get_project_id(proj_key):
+    """
+    :param proj_key: Send ticket ID to get the project ID
+    :return: Extract the result from jira to get the project ID
+    """
     try:
-
         url = f"https://{JIRA_DOMAIN}.atlassian.net/rest/api/2/project"
-        headers ={"Authorization": "Basic c3VyeWEubXIramlyYTFAMTBkZWNvZGVycy5pbjpnRTByeFptRzBEalQ0SXo3TkU4YUYzM0Y=",
+        headers ={"Authorization": f"Basic {AUTH_TOKEN}",
                   "Content-Type": "application/json"}
         response = requests.request(url=url, method="GET", headers=headers)
         for res in json.loads(response.content):
@@ -267,9 +272,14 @@ def get_project_id(proj_key):
 
 
 def get_issue_type_id(proj_key, issue_type):
+    """
+    :param proj_key: Project ID to know about the project Details
+    :param issue_type: Issue type to create a test execution ticket
+    :return: Sends the Extract issue type value for jira ticket
+    """
     try:
         url = f"https://{JIRA_DOMAIN}.atlassian.net/rest/api/2/project/{proj_key}"
-        headers = {"Authorization": "Basic c3VyeWEubXIramlyYTFAMTBkZWNvZGVycy5pbjpnRTByeFptRzBEalQ0SXo3TkU4YUYzM0Y=",
+        headers = {"Authorization": f"Basic {AUTH_TOKEN}",
                    "Content-Type": "application/json"}
         response = requests.request(url=url, method="GET", headers=headers)
         issues = json.loads(response.content)
@@ -279,24 +289,22 @@ def get_issue_type_id(proj_key, issue_type):
                     return issue['id']
         else:
             raise Exception("No issue types in response")
-
     except Exception as e:
-        logger.info("Exception occured in getting issue type id", e)
+        logger.info("Exception occurred in getting issue type value", e)
 
 
-def send_results_to_jira(result, ticket_id):
+def send_results_to_jira(result, ticket_id, test_execution_id):
     global testcase_comment
 
     # If testcase_dict status is skipped, then it belongs to XFail condition, which got failed
     testcase_dict = {"passed": "Passed", "failed": "Failed", "skipped": "Xfailed"}
 
-
-    testcase_comment = f"{ticket_id} - Testcase is " + str(testcase_dict[result.outcome])
+    testcase_comment = f"{ticket_id} - Testcase is " + str(testcase_dict[result]) + f" in Test execution ticket ID - {test_execution_id}\n"
     comment_url = f"https://{JIRA_DOMAIN}{jira_api_endpoint}/{ticket_id}/comment"
 
     payload = json.dumps({"body": testcase_comment})
     headers = {
-        'Authorization': F'Basic {AUTH_TOKEN}',
+        'Authorization': f'Basic {AUTH_TOKEN}',
         'Content-Type': 'application/json'
     }
 
@@ -306,8 +314,8 @@ def send_results_to_jira(result, ticket_id):
 
     # Below post call is to make transition for the JIRA tickets
     # URL : https://testautomatejira.atlassian.net/rest/api/2/issue/{ticket_id}/transitions"
-    transition_url = f"https://{JIRA_DOMAIN}{jira_api_endpoint}/{ticket_id}/transitions"
-    apply_transition_to_jira_tickets(transition_url, headers=headers, status=result.outcome)
+    # transition_url = f"https://{JIRA_DOMAIN}{jira_api_endpoint}/{ticket_id}/transitions"
+    # apply_transition_to_jira_tickets(transition_url, headers=headers, status=result)
 
 
 def apply_transition_to_jira_tickets(transition_url, headers, status):
@@ -345,65 +353,66 @@ def _capture_screenshot(report):
     if driver:
         try:
             attach(data=driver.get_screenshot_as_png())
-            if JIRA_CONDITION:
-                doc_path, ticket_id = save_failed_details_on_dir(report)
-                send_failed_doc_to_jira(ticket_id, doc_path)
+            # if JIRA_CONDITION:
+            #     doc_path, ticket_id = save_failed_details_on_dir(report)
+            #     send_failed_doc_to_jira(ticket_id, doc_path)
         except Exception as e:
             logging.error(f'{error_msg}  Exception: f{str(e)}')
     else:
         logging.error(f'{error_msg}  Selenium driver not valid.')
 
 
-def save_failed_details_on_dir(report):
-    ticket_id = f"{PREFIX_TICKET_VALUE}" + \
-                (report.nodeid.replace("_", "-")).split(f"{PREFIX_TICKET_VALUE}")[1][:-1]
-    report_time = datetime.now()
-    timestamp = datetime.strftime(report_time, '%Y_%m_%d_%H_%M_%S')
-    file_exact_path = os.path.abspath(__file__).split("/conftest.py")[0]
-    img_path = "jira_failed_images"
-    if img_path not in os.listdir(path=file_exact_path):
-        os.makedirs(file_exact_path+"/"+img_path)
-    img_path += f"/{ticket_id}_img_{timestamp}.png"
-    driver.save_screenshot(file_exact_path+"/"+img_path)
-    doc_path = create_document_for_failed_case(file_exact_path+"/"+img_path, ticket_id)
-    return doc_path, ticket_id
-
-
-def create_document_for_failed_case(path, ticket_id):
-    doc = DocumentCreation()
-    doc.create_para_object()
-    doc.add_run_to_para_obj()
-    doc.add_text_to_run_obj(f"TICKET ID - {ticket_id}")
-    doc.create_para_object()
-    doc.add_run_to_para_obj()
-    doc.add_text_to_run_obj("This ticket is failed at the Below point of execution")
-    doc.create_para_object()
-    doc.add_run_to_para_obj()
-    doc.add_text_to_run_obj("Screenshot :")
-    doc.add_image_to_run_obj(path)
-    doc_path = path.replace("jira_failed_images", "jira_failed_docs")
-    doc_path = doc_path.replace("_img_", "_doc_")
-    doc_path = doc_path.replace(".png", ".docx")
-    file_exact_path = os.path.abspath(__file__).split("/conftest.py")[0]
-    path = "jira_failed_docs"
-    if path not in os.listdir(path=file_exact_path):
-        os.makedirs(file_exact_path+"/"+path)
-    doc.save_document(doc_path)
-    return doc_path
-
-
-def send_failed_doc_to_jira(ticket_id, doc_path):
-    doc_url = f"https://{JIRA_DOMAIN}{jira_api_endpoint}/{ticket_id}/attachments"
-    file_type = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    timestamp = datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M')
-    files = [('file', (f'{ticket_id}_failed_doc_result_at_{timestamp}.docx', open(f'{doc_path}', 'rb'), file_type))]
-    headers = {
-        'X-Atlassian-Token': 'no-check',
-        'Authorization': f'Basic {AUTH_TOKEN}'
-    }
-
-    # Below post call is used to add the document on JIRA tickets
-    # URL : https://testautomatejira.atlassian.net/rest/api/2/issue/{ticket_id}/attachments"
-    requests.request("POST", doc_url, headers=headers, files=files)
+# def save_failed_details_on_dir(report):
+#     # ticket_id = f"{PREFIX_TICKET_VALUE}" + \
+#     #             (report.nodeid.replace("_", "-")).split(f"{PREFIX_TICKET_VALUE}")[1][:-1]
+#     ticket_id = ""
+#     report_time = datetime.now()
+#     timestamp = datetime.strftime(report_time, '%Y_%m_%d_%H_%M_%S')
+#     file_exact_path = os.path.abspath(__file__).split("/conftest.py")[0]
+#     img_path = "jira_failed_images"
+#     if img_path not in os.listdir(path=file_exact_path):
+#         os.makedirs(file_exact_path+"/"+img_path)
+#     img_path += f"/{ticket_id}_img_{timestamp}.png"
+#     driver.save_screenshot(file_exact_path+"/"+img_path)
+#     doc_path = create_document_for_failed_case(file_exact_path+"/"+img_path, ticket_id)
+#     return doc_path, ticket_id
+#
+#
+# def create_document_for_failed_case(path, ticket_id):
+#     doc = DocumentCreation()
+#     doc.create_para_object()
+#     doc.add_run_to_para_obj()
+#     doc.add_text_to_run_obj(f"TICKET ID - {ticket_id}")
+#     doc.create_para_object()
+#     doc.add_run_to_para_obj()
+#     doc.add_text_to_run_obj("This ticket is failed at the Below point of execution")
+#     doc.create_para_object()
+#     doc.add_run_to_para_obj()
+#     doc.add_text_to_run_obj("Screenshot :")
+#     doc.add_image_to_run_obj(path)
+#     doc_path = path.replace("jira_failed_images", "jira_failed_docs")
+#     doc_path = doc_path.replace("_img_", "_doc_")
+#     doc_path = doc_path.replace(".png", ".docx")
+#     file_exact_path = os.path.abspath(__file__).split("/conftest.py")[0]
+#     path = "jira_failed_docs"
+#     if path not in os.listdir(path=file_exact_path):
+#         os.makedirs(file_exact_path+"/"+path)
+#     doc.save_document(doc_path)
+#     return doc_path
+#
+#
+# def send_failed_doc_to_jira(ticket_id, doc_path):
+#     doc_url = f"https://{JIRA_DOMAIN}{jira_api_endpoint}/{ticket_id}/attachments"
+#     file_type = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+#     timestamp = datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M')
+#     files = [('file', (f'{ticket_id}_failed_doc_result_at_{timestamp}.docx', open(f'{doc_path}', 'rb'), file_type))]
+#     headers = {
+#         'X-Atlassian-Token': 'no-check',
+#         'Authorization': f'Basic {AUTH_TOKEN}'
+#     }
+#
+#     # Below post call is used to add the document on JIRA tickets
+#     # URL : https://testautomatejira.atlassian.net/rest/api/2/issue/{ticket_id}/attachments"
+#     requests.request("POST", doc_url, headers=headers, files=files)
 
 
--- test_cases/ui/test_report_case.py
+++ test_cases/ui/test_report_case.py
@@ -3,7 +3,6 @@ import pytest
 import logging
 from selenium.webdriver.common.by import By
 from . import JiraClientInit
-import inspect
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
@@ -12,14 +11,22 @@ logger = logging.getLogger(__name__)
 
 class TestCaseJiraAddDoc:
 
+    @pytest.mark.parametrize("ticket_id", ["LB-1"])
+    def test_search_with_valid_value_1(self, ticket_id):
+        assert 1 == 1
+
+    @pytest.mark.parametrize("ticket_id", ["TP-1"])
+    def test_search_with_valid_value_3(self, ticket_id):
+        assert 1 == 1
+
+    @pytest.mark.parametrize("ticket_id", ["LB-2"])
+    def test_search_with_valid_value_2(self, ticket_id):
+        assert 1 == 1
+
+    @pytest.mark.parametrize("ticket_id", ["TP-2"])
+    def test_search_with_valid_value_4(self, ticket_id):
+        assert 1 == 2
+
     @pytest.mark.parametrize("ticket_id", ["LB-6"])
-    def test_search_with_valid_value_TP_1(self, ticket_id):
-        assert 1 == 1
-
-    @pytest.mark.parametrize("ticket_id", ["LB-7"])
-    def test_search_with_valid_value_TP_2(self, ticket_id):
-        assert 1 == 1
-
-    @pytest.mark.parametrize("ticket_id", ["TP-40"])
-    def test_search_with_valid_value_TP_3(self, ticket_id):
-        assert 1 == 1
+    def test_search_with_valid_value_5(self, ticket_id):
+        assert 1 == 2
